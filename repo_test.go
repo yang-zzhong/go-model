4de7ac43da4d2dd053646333758c06e78f66ed38
hackyoung <hackyoung@163.com>
2018-03-22 18:28:27 +0800

    重命名文件

---
+++ repo_test.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	. "github.com/yang-zzhong/go-querybuilder"
+	."testing"
+)
+
+var con *sql.DB
+
+type User struct {
+	Id        string
+	Name      string
+	Age       int
+	Level     int
+	Optional  string
+	CreatedAt []uint8			// time.Time
+}
+
+func (u *User) TableName() string {
+	return "users"
+}
+
+func (u *User) IdKey() interface{} {
+	return "id"
+}
+
+func init() {
+	var err error
+	con, err = sql.Open("mysql", "root:young159357789@/test_go")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestRepo(t * T) {
+	repo := NewRepo(&User{}, con, &MysqlPlaceholder{})
+	repo.Where("name", LIKE, "yang%")
+	for _, item := range repo.Fetch() {
+		fmt.Println(item)
+	}
+}
