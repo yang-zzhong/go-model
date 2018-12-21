3cce8074241303803827b7ce36c216ce10aad900
hackyoung <hackyoung@163.com>
2018-12-21 12:19:20 +0800

    修改

--- connection.go
+++ connection.go
@@ -4,58 +4,224 @@ import (
 	"context"
 	"database/sql"
 	. "github.com/yang-zzhong/go-querybuilder"
+	"sync"
 )
 
 var (
-	Conn     *Connection
+	instance *Db
+	once     sync.Once
+	db       *sql.DB
 	modifier Modifier
-	inited   bool
 )
 
 // tx callback type
 type txhandler func(t *sql.Tx) error
 
-type Connection struct {
+type Db struct {
+	txs []*sql.Tx
 	*sql.DB
 }
 
-func Config(db *sql.DB, m Modifier) {
-	Conn = NewConn(db)
+func Config(sdb *sql.DB, m Modifier) {
+	db = sdb
 	modifier = m
-	inited = true
 }
 
-func NewConn(db *sql.DB) *Connection {
-	conn := new(Connection)
-	conn.DB = db
+func GetDB() *Db {
+	once.Do(func() {
+		instance = new(Db)
+		instance.DB = db
+		instance.tx = []*sql.Tx{}
+	})
 
-	return conn
+	return instance
 }
 
-func (conn *Connection) Tx(handle txhandler, ctx context.Context, opts *sql.TxOptions) error {
-	if ctx == nil {
-		nctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-		ctx = nctx
-	}
-	tx, err := conn.DB.BeginTx(ctx, opts)
+func (db *Db) TxContext(handle txhandler, ctx context.Context, opts *sql.TxOptions) error {
+	var err error
+	db.BeginTx(ctx, opts)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if e := recover(); e != nil {
-			tx.Rollback()
+			db.Rollback()
 			panic(e)
 		}
 	}()
 	if err := handle(tx); err != nil {
-		tx.Rollback()
+		db.Rollback()
 		return err
 	}
-	if err := tx.Commit(); err != nil {
-		tx.Rollback()
+	if err := db.Commit(); err != nil {
+		db.Rollback()
 		return err
 	}
 
 	return nil
+}
+
+func (db *Db) Tx(handle txhandler) error {
+	var err error
+	db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := recover(); e != nil {
+			db.Rollback()
+			panic(e)
+		}
+	}()
+	if err := handle(tx); err != nil {
+		db.Rollback()
+		return err
+	}
+	if err := db.Commit(); err != nil {
+		db.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (db *Db) Begin() error {
+	var err error
+	db.tx, err = db.DB.Begin()
+
+	return err
+}
+
+func (db *Db) BeginTx(ctx context.Context, opts *sql.TxOptions) error {
+	var err error
+	tx, err = db.DB.BeginTx(ctx, opts)
+	db.txs = append(db.txs, tx)
+
+	return err
+}
+
+func (db *Db) Commit() error {
+	tx = db.tx()
+	if tx != nil {
+		panic("please begin before commit")
+	}
+	err := db.tx.Commit()
+	db.poptx()
+	return err
+}
+
+func (db *Db) Rollback() error {
+	tx = db.tx()
+	if tx != nil {
+		panic("please begin before rollback")
+	}
+	err := db.tx.Rollback()
+	db.poptx()
+	db.tx = nil
+}
+
+func (db *Db) Exec(query string, args ...interface{}) (sql.Result, error) {
+	tx = db.tx()
+	if tx != nil {
+		return db.tx.Exec(query, args)
+	}
+
+	return db.DB.Exec(query, args)
+}
+
+func (db *Db) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	tx = db.tx()
+	if tx != nil {
+		return db.tx.ExecContext(ctx, query, args)
+	}
+
+	return db.DB.Exec(ctx, query, args)
+}
+
+func (db *Db) Prepare(query string) (*Stmt, error) {
+	tx = db.tx()
+	if tx != nil {
+		return db.tx.Prepare(query)
+	}
+
+	return db.DB.Prepare(query)
+}
+
+func (db *Db) PrepareContext(ctx context.Context, query string) (*Stmt, error) {
+	tx = db.tx()
+	if tx != nil {
+		return db.tx.PrepareContext(query)
+	}
+
+	return db.DB.PrepareContext(query)
+}
+
+func (db *Db) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	tx = db.tx()
+	if tx != nil {
+		return db.tx.Query(query, args)
+	}
+
+	return db.DB.Query(query, args)
+}
+
+func (db *Db) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	tx = db.tx()
+	if tx != nil {
+		return db.tx.QueryContext(ctx, query, args)
+	}
+
+	return db.DB.QueryContext(ctx, query, args)
+}
+
+func (db *Db) QueryRow(query string, args ...interface{}) *sql.Row {
+	tx = db.tx()
+	if tx != nil {
+		return db.tx.QueryRow(query, args)
+	}
+
+	return db.DB.QueryRow(query, args)
+}
+
+func (db *Db) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
+	tx = db.tx()
+	if tx != nil {
+		return tx.QueryRowContext(ctx, query, args)
+	}
+
+	return db.DB.QueryRowContext(ctx, query, args)
+}
+
+func (db *Db) Stmt() *sql.Stmt {
+	tx = db.tx()
+	if tx != nil {
+		return tx.Stmt()
+	}
+
+	return db.DB.Stmt()
+}
+
+func (db *Db) StmtContext(ctx context.Context) *sql.Stmt {
+	tx := db.tx()
+	if tx != nil {
+		return tx.StmtContext(ctx)
+	}
+
+	return db.DB.StmtContext(ctx)
+}
+
+func (db *Db) tx() *sql.Tx {
+	if len(db.txs) == 0 {
+		return nil
+	}
+
+	return db.txs[len(db.txs)-1]
+}
+
+func (db *Db) poptx() {
+	if len(db.txs) == 1 {
+		db.txs = []*sql.Tx{}
+		return
+	}
+	db.txs = db.txs[:len(db.txs)-2]
 }
--- model_repo.go
+++ model_repo.go
@@ -32,21 +32,18 @@ type with struct {
 // repo
 type Repo struct {
 	model    interface{} // repo row model
-	conn     *Connection // db
 	modifier Modifier    // sql modifier
 	oncreate modify      // on create callback
 	onupdate modify      // on update callback
 	ondelete modify      // on delete callback
 	withs    []with      // maintain fetch model relationship
-	tx       *sql.Tx     // tx
 	*Builder
 }
 
 // new custom repo
-func NewCustomRepo(m interface{}, conn *Connection, p Modifier) *Repo {
+func NewCustomRepo(m interface{}, p Modifier) *Repo {
 	repo := new(Repo)
 	repo.model = m
-	repo.conn = conn
 	repo.modifier = p
 	repo.oncreate = func(_ interface{}) error { return nil }
 	repo.onupdate = func(_ interface{}) error { return nil }
@@ -58,25 +55,9 @@ func NewCustomRepo(m interface{}, conn *Connection, p Modifier) *Repo {
 	return repo
 }
 
-func (repo *Repo) WithTx(tx *sql.Tx) *Repo {
-	repo.tx = tx
-
-	return repo
-}
-
-func (repo *Repo) WithoutTx() *Repo {
-	repo.tx = nil
-
-	return repo
-}
-
 // new default repo
 func NewRepo(m interface{}) (repo *Repo, err error) {
-	if !inited {
-		err = errors.New("not config the db and modifier yet")
-		return
-	}
-	repo = NewCustomRepo(m, Conn, modifier)
+	repo = NewCustomRepo(m, modifier)
 	return
 }
 
@@ -105,7 +86,11 @@ func (repo *Repo) Clean() {
 
 // count
 func (repo *Repo) Count() (int, error) {
-	rows, err := repo.conn.Query(repo.ForCount(), repo.Params()...)
+	return repo.CountDB(GetDB())
+}
+
+func (repo *Repo) CountDB(db DB) (int, error) {
+	rows, err := db.Query(repo.ForCount(), repo.Params()...)
 	if err != nil {
 		return 0, err
 	}
@@ -128,13 +113,16 @@ func (repo *Repo) MustCount() int {
 }
 
 func (repo *Repo) Query(handle rowshandler) error {
-	var rows *sql.Rows
-	var cols []string
-	var err error
-	if rows, err = repo.query(); err != nil {
+	return repo.QueryDB(handle, GetDB())
+}
+
+func (repo *Repo) QueryDB(handle rowshandler, db DB) error {
+	rows, err := db.Query(repo.ForCount(), repo.Params()...)
+	if err != nil {
 		return err
 	}
 	defer rows.Close()
+	var cols []string
 	if cols, err = rows.Columns(); err != nil {
 		return err
 	}
@@ -283,106 +271,83 @@ func (repo *Repo) DeleteRaw() error {
 	return err
 }
 
-func (repo *Repo) Update(models interface{}) error {
-	val := reflect.ValueOf(models)
-	for val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-	r := NewCustomRepo(repo.model, repo.conn, repo.modifier)
-	r.tx = repo.tx
-	field := repo.model.(Model).PK()
+func (repo *Repo) UpdateSlice(models []interface{}, db DB) error {
 	mm := repo.model.(Mapable).Mapper()
-	switch val.Kind() {
-	case reflect.Struct:
-		if v, err := mm.colValue(models, field); err == nil {
-			if err := repo.onupdate(models); err != nil {
-				return err
-			}
-			r.Where(field, v)
-			return r.UpdateRaw(mm.extract(models))
-		} else {
+	field := repo.model.(Model).PK()
+	ids := []interface{}{}
+	for _, m := range models {
+		v, err := mm.colValue(m, field)
+		if err != nil {
 			return err
 		}
-	case reflect.Slice:
-		slice := models.([]interface{})
-		for _, m := range slice {
-			if v, err := mm.colValue(m, field); err == nil {
-				if err := repo.onupdate(m); err != nil {
-					return err
-				}
-				r.Where(field, v)
-				if err := r.UpdateRaw(mm.extract(m)); err != nil {
-					return err
-				}
-			} else {
-				return err
-			}
+		ids = append(ids, v)
+		if err := repo.onupdate(m); err != nil {
+			return err
 		}
-		return nil
-	case reflect.Map:
-		maps := models.([]interface{})
-		for _, m := range maps {
-			if v, err := mm.colValue(m, field); err == nil {
-				if err := repo.onupdate(m); err != nil {
-					return err
-				}
-				r.Where(field, v)
-				if err := r.UpdateRaw(mm.extract(m)); err != nil {
-					return err
-				}
-			} else {
-				return err
-			}
-		}
-		return nil
 	}
+	if len(ids) > 0 {
+		repo.WhereIn(field, ids)
+		if err := r.UpdateRaw(mm.extract(m)); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
-func (repo *Repo) Create(models interface{}) error {
-	var err error
-	var data []map[string]interface{}
-	val := reflect.ValueOf(models)
-	for val.Kind() == reflect.Ptr {
-		val = val.Elem()
+func (repo *Repo) Update(model interface{}) error {
+	v, err := mm.colValue(model, field)
+	if err != nil {
+		return err
 	}
-	ms := []interface{}{}
-	switch val.Kind() {
-	case reflect.Struct:
-		if err := repo.oncreate(models); err != nil {
+	if err := repo.onupdate(model); err != nil {
+		return err
+	}
+	r.Where(field, v)
+
+	return r.UpdateRaw(mm.extract(model))
+}
+
+func (repo *Repo) CreateSlice(models []interface{}) error {
+	return repo.CreateSliceDB(models, GetDB())
+}
+
+func (repo *Repo) CreateSliceDB(models []interface{}, db DB) error {
+	r := NewCustomRepo(repo.model, repo.modifier)
+	var data []map[string]interface{}
+	for _, m := range models {
+		if err := repo.oncreate(m); err != nil {
 			return err
 		}
-		data = append(data, repo.model.(Mapable).Mapper().extract(models))
-		ms = append(ms, models)
-	case reflect.Slice:
-		slice := val.Interface().([]interface{})
-		for _, m := range slice {
-			if err := repo.oncreate(m); err != nil {
-				return err
-			}
-			data = append(data, repo.model.(Mapable).Mapper().extract(m))
-			ms = append(ms, m)
-		}
-	case reflect.Map:
-		maps := val.Interface().(map[interface{}]interface{})
-		for _, m := range maps {
-			if err := repo.oncreate(m); err != nil {
-				return err
-			}
-			data = append(data, repo.model.(Mapable).Mapper().extract(m))
-			ms = append(ms, m)
-		}
+		data = append(data, repo.model.(Mapable).Mapper().extract(m))
 	}
-	r := NewCustomRepo(repo.model, repo.conn, repo.modifier)
-	r.tx = repo.tx
-	_, err = r.exec(r.ForInsert(data))
-	if err == nil {
-		for _, m := range ms {
-			m.(Model).SetFresh(false)
-		}
+	if _, err := db.Exec(r.ForInsert(data), r.Params()...); err != nil {
+		return err
+	}
+	for _, m := range models {
+		m.(Model).SetFresh(false)
 	}
 
-	return err
+	return nil
+}
+
+func (repo *Repo) Create(model interface{}) error {
+	return repo.CreateDB(model, GetDB())
+}
+
+func (repo *Repo) CreateDB(model interface{}, db DB) error {
+	var data []map[string]interface{}
+	if err := repo.oncreate(models); err != nil {
+		return err
+	}
+	data = append(data, repo.model.(Mapable).Mapper().extract(model))
+	r := NewCustomRepo(repo.model, repo.modifier)
+	if _, err := db.Exec(r.ForInsert(data), r.Params()...); err != nil {
+		return err
+	}
+	m.(Model).SetFresh(false)
+
+	return nil
 }
 
 func (repo *Repo) Delete(models interface{}) error {
@@ -449,48 +414,48 @@ func (repo *Repo) Delete(models interface{}) error {
 }
 
 func (repo *Repo) exec(sql string) (res sql.Result, err error) {
-	defer func() {
-		if e := recover(); e != nil {
-			switch e.(type) {
-			case string:
-				err = &Error{ERR_SQL, errors.New(e.(string))}
-			case error:
-				err = &Error{ERR_SQL, e.(error)}
-			}
-		}
-	}()
-	if repo.tx != nil {
-		if r, e := repo.tx.Exec(sql, repo.Params()...); e != nil {
-			err = &Error{ERR_SQL, e}
-		} else {
-			res = r
-		}
-	} else {
-		if r, e := repo.conn.Exec(sql, repo.Params()...); e != nil {
-			err = &Error{ERR_SQL, e}
-		} else {
-			res = r
-		}
-	}
-	return
+	// defer func() {
+	// 	if e := recover(); e != nil {
+	// 		switch e.(type) {
+	// 		case string:
+	// 			err = &Error{ERR_SQL, errors.New(e.(string))}
+	// 		case error:
+	// 			err = &Error{ERR_SQL, e.(error)}
+	// 		}
+	// 	}
+	// }()
+	// if repo.tx != nil {
+	// 	if r, e := repo.tx.Exec(sql, repo.Params()...); e != nil {
+	// 		err = &Error{ERR_SQL, e}
+	// 	} else {
+	// 		res = r
+	// 	}
+	// } else {
+	// 	if r, e := repo.db.Exec(sql, repo.Params()...); e != nil {
+	// 		err = &Error{ERR_SQL, e}
+	// 	} else {
+	// 		res = r
+	// 	}
+	// }
+	// return
 }
 
 func (repo *Repo) query() (rows *sql.Rows, err error) {
-	defer func() {
-		if e := recover(); e != nil {
-			switch e.(type) {
-			case string:
-				err = &Error{ERR_SQL, errors.New(e.(string))}
-			case error:
-				err = &Error{ERR_SQL, e.(error)}
-			}
-		}
-	}()
-	if r, e := repo.conn.Query(repo.ForQuery(), repo.Params()...); e != nil {
-		err = &Error{ERR_SQL, e}
-	} else {
-		rows = r
-	}
+	// defer func() {
+	// 	if e := recover(); e != nil {
+	// 		switch e.(type) {
+	// 		case string:
+	// 			err = &Error{ERR_SQL, errors.New(e.(string))}
+	// 		case error:
+	// 			err = &Error{ERR_SQL, e.(error)}
+	// 		}
+	// 	}
+	// }()
+	// if r, e := repo.conn.Query(repo.ForQuery(), repo.Params()...); e != nil {
+	// 	err = &Error{ERR_SQL, e}
+	// } else {
+	// 	rows = r
+	// }
 
-	return
+	// return
 }
